6a1b094f80528fcca492c6ede82a085b96ba3c71
galhrrera <juan.galeanoherrera@gmail.com>
2023-11-08 22:35:04 -0500

    Se creó el minijuego adivinar número

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -183,6 +183,7 @@ var pixelArt = document.getElementById("pixel-art");
 var memoria = document.getElementById("memoria");
 var ahorcado = document.getElementById("ahorcado");
 var menuToAdivinarNumeroBtn = document.getElementById("adivinar-numero-btn");
+var adivinarBtn = document.getElementById("adivinar-btn");
 window.onload = function () {
   hideAllSections();
   setTimeout(function () {
@@ -209,6 +210,43 @@ document.addEventListener('DOMContentLoaded', function () {
     // loadDreams();
   });
 });
+
+/* ADIVINANZA */
+var adivinanzaMsg1 = document.getElementById("adivinar-numero-mensaje1");
+var adivinanzaMsg2 = document.getElementById("adivinar-numero-mensaje2");
+var adivinanzaMsg3 = document.getElementById("adivinar-numero-mensaje3");
+var adivinanzaRespuesta = Math.floor(Math.random() * 100) + 1;
+var noDeIntentos = 0;
+var nosIntenados = [];
+document.addEventListener('DOMContentLoaded', function () {
+  adivinarBtn.addEventListener('click', function () {
+    var adivinanzaUsuario = document.getElementById("adivinanza").value;
+    if (adivinanzaUsuario < 1 || adivinanzaUsuario > 100) {
+      alert("Ingresa un número entre 1 y 100");
+    } else {
+      nosIntenados.push(adivinanzaUsuario);
+      noDeIntentos += 1;
+      if (adivinanzaUsuario < adivinanzaRespuesta) {
+        adivinanzaMsg1.textContent = "Tu número intentado es muy bajo";
+        adivinanzaMsg2.textContent = "N° de intentos: " + noDeIntentos;
+        adivinanzaMsg3.textContent = "Núemros intentados: " + nosIntenados;
+      } else if (adivinanzaUsuario > adivinanzaRespuesta) {
+        adivinanzaMsg1.textContent = "Tu número intentado es muy alto";
+        adivinanzaMsg2.textContent = "N° de intentos: " + noDeIntentos;
+        adivinanzaMsg3.textContent = "Núemros intentados: " + nosIntenados;
+      } else if (adivinanzaUsuario == adivinanzaRespuesta) {
+        adivinanzaMsg1.textContent = "¡HAS GANADO!";
+        adivinanzaMsg2.textContent = "El número era: " + adivinanzaRespuesta;
+        adivinanzaMsg3.textContent = "Número de intentos: " + noDeIntentos;
+        document.getElementById("adivinar-btn").disable = true;
+      }
+    }
+  });
+});
+
+// function playAdivinanza() {
+
+// }
 },{"./styles/main.scss":"styles/main.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const memoria = document.getElementById("memoria");
 const ahorcado = document.getElementById("ahorcado");
 
 const menuToAdivinarNumeroBtn = document.getElementById("adivinar-numero-btn");
+const adivinarBtn = document.getElementById("adivinar-btn");
 
 window.onload = function () {
     hideAllSections();
@@ -37,3 +38,47 @@ document.addEventListener('DOMContentLoaded', function () {
         // loadDreams();
     });
 });
+
+/* ADIVINANZA */
+var adivinanzaMsg1 = document.getElementById("adivinar-numero-mensaje1");
+var adivinanzaMsg2 = document.getElementById("adivinar-numero-mensaje2");
+var adivinanzaMsg3 = document.getElementById("adivinar-numero-mensaje3");
+
+var adivinanzaRespuesta = Math.floor(Math.random() * 100) + 1;
+var noDeIntentos = 0;
+var nosIntenados = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    adivinarBtn.addEventListener('click', function () {
+        var adivinanzaUsuario = document.getElementById("adivinanza").value;
+
+        if (adivinanzaUsuario < 1 || adivinanzaUsuario > 100) {
+            alert("Ingresa un número entre 1 y 100");
+        }
+        else {
+            nosIntenados.push(adivinanzaUsuario);
+            noDeIntentos += 1;
+
+            if (adivinanzaUsuario < adivinanzaRespuesta) {
+                adivinanzaMsg1.textContent = "Tu número intentado es muy bajo";
+                adivinanzaMsg2.textContent = "N° de intentos: " + noDeIntentos;
+                adivinanzaMsg3.textContent = "Núemros intentados: " + nosIntenados;
+            }
+            else if (adivinanzaUsuario > adivinanzaRespuesta) {
+                adivinanzaMsg1.textContent = "Tu número intentado es muy alto";
+                adivinanzaMsg2.textContent = "N° de intentos: " + noDeIntentos;
+                adivinanzaMsg3.textContent = "Núemros intentados: " + nosIntenados;
+            }
+            else if (adivinanzaUsuario == adivinanzaRespuesta){
+                adivinanzaMsg1.textContent = "¡HAS GANADO!";
+                adivinanzaMsg2.textContent = "El número era: " + adivinanzaRespuesta;
+                adivinanzaMsg3.textContent = "Número de intentos: " + noDeIntentos;
+                document.getElementById("adivinar-btn").disable = true;
+            }
+        }
+    })
+})
+
+// function playAdivinanza() {
+
+// }
